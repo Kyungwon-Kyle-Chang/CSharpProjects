8e26f7aaaa855759c858613941cc5d19f1567cf9
Kyungwon Chang <kwmnusa@naver.com>
2017-10-17 09:22:35 +0900

    ColorDetectMacro Re-Commit

---
+++ ColorDetectMacro/ColorDetectMacro/MainWindow.xaml.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorDetectMacro
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextBox[] _textBoxArray = new TextBox[8];
+        private int _textBoxArrayIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _textBoxArray[0] = RefreshX;
+            _textBoxArray[1] = RefreshY;
+            _textBoxArray[2] = VIP_StartX;
+            _textBoxArray[3] = VIP_StartY;
+            _textBoxArray[4] = VIP_EndX;
+            _textBoxArray[5] = VIP_EndY;
+            _textBoxArray[6] = ProceedX;
+            _textBoxArray[7] = ProceedY;
+
+            MouseHook.Start();
+        }
+
+        private void MouseHook_MouseLClick(object sender, EventArgs e)
+        {
+            _textBoxArray[_textBoxArrayIndex].Text = GetCursorPos().X.ToString();
+            _textBoxArray[_textBoxArrayIndex + 1].Text = GetCursorPos().Y.ToString();
+
+            _textBoxArrayIndex += 2;
+            if (_textBoxArrayIndex >= 8)
+            {
+                MouseHook.MouseLClick -= MouseHook_MouseLClick;
+                _textBoxArrayIndex = 0;
+            }
+
+        }
+
+        private Point GetCursorPos()
+        {
+            Win32Point winPoint = new Win32Point();
+            GetCursorPos(ref winPoint);
+            Point point = ConvertPixelsToUnits(winPoint.X, winPoint.Y);
+            return point;
+        }
+
+
+        private bool CheckColor()
+        {
+            return false;
+        }
+        //------------------------------------ UI Functions ----------------------------------
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            MouseHook.MouseLClick += new EventHandler(MouseHook_MouseLClick);
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x, y;
+
+            x = Convert.ToInt32(_textBoxArray[0].Text);
+            y = Convert.ToInt32(_textBoxArray[1].Text);
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)x, (uint)y, 0, 0);
+
+            if (!CheckColor()) return;
+
+            //IntPtr hWnd = FindWindow("Notepad", null);
+
+            //// If found, position it.
+            //if (hWnd != IntPtr.Zero)
+            //{
+            //    // Move the window to (0,0) without changing its size or position
+            //    // in the Z order.
+            //    SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            //}
+        }
+        //-------------------------------------------------------------------------------------
+        #region MouseHook Class
+        public static class MouseHook
+        {
+            public static event EventHandler MouseMove = delegate { };
+            public static event EventHandler MouseLClick = delegate { };
+
+            public static void Start()
+            {
+                _hookID = SetHook(_proc);
+            }
+
+            public static void stop()
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+
+            private static LowLevelMouseProc _proc = HookCallback;
+            private static IntPtr _hookID = IntPtr.Zero;
+
+            private static IntPtr SetHook(LowLevelMouseProc proc)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                        GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+
+            private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            private static IntPtr HookCallback(
+                int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
+                {
+                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                    MouseMove(null, new EventArgs());
+                }
+                else if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+                {
+                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                    MouseLClick(null, new EventArgs());
+                }
+                return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            }
+
+            private const int WH_MOUSE_LL = 14;
+
+            private enum MouseMessages
+            {
+                WM_LBUTTONDOWN = 0x0201,
+                WM_LBUTTONUP = 0x0202,
+                WM_MOUSEMOVE = 0x0200,
+                WM_MOUSEWHEEL = 0x020A,
+                WM_RBUTTONDOWN = 0x0204,
+                WM_RBUTTONUP = 0x0205
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct POINT
+            {
+                public int x;
+                public int y;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct MSLLHOOKSTRUCT
+            {
+                public POINT pt;
+                public uint mouseData;
+                public uint flags;
+                public uint time;
+                public IntPtr dwExtraInfo;
+            }
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook,
+                LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+                IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+        #endregion
+
+        #region Native Methods
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [DllImport("User32", EntryPoint = "SetCursorPos")]
+        public static extern void SetCursorPos(int x, int y);
+
+        private Point ConvertPixelsToUnits(int x, int y)
+        {
+            // get the system DPI
+            IntPtr dDC = GetDC(IntPtr.Zero); // Get desktop DC
+            int dpi = GetDeviceCaps(dDC, 88);
+            bool rv = ReleaseDC(IntPtr.Zero, dDC);
+
+            // WPF's physical unit size is calculated by taking the 
+            // "Device-Independant Unit Size" (always 1/96)
+            // and scaling it by the system DPI
+            double physicalUnitSize = (1d / 96d) * (double)dpi;
+            Point wpfUnits = new Point(physicalUnitSize * (double)x,
+                physicalUnitSize * (double)y);
+
+            return wpfUnits;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        public static extern int FindWindowEx(int hwndParent, int hwndEnfant, int lpClasse, string lpTitre);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOZORDER = 0x0004;
+
+        #endregion
+    }
+    
+
+}
